55fff4bc8e9545b527270af4f3761f964675bb2c
Erno Viitanen <erno.viitanen@student.lut.fi>
2020-10-21 19:51:52 +0300

    Level 6

--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -4,40 +4,45 @@ using UnityEngine;
 
 public class WorldController : MonoBehaviour
 {
-    
-    public Material targetMaterial;
-    public Material hiddenMaterial;
-    // Start is called before the first frame update
-    GameObject target;
-    void Start()
-    {
-        // assign the material to the renderer
-                
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!target)
-        {            
-            target = GameObject.FindGameObjectWithTag("HexTarget");
-            //Debug.Log(target.name);
- 
-            target.GetComponent<Renderer>().material = targetMaterial;   
-            MeshCollider collider = target.GetComponent<MeshCollider>();
-            collider.convex = true;
-            collider.isTrigger = true;
+   public Material targetMaterial;
+   public Material hiddenMaterial;
+   // Start is called before the first frame update
+   GameObject target;
+   void Start()
+   {
+      // assign the material to the renderer
 
-            foreach (var item in GameObject.FindGameObjectsWithTag("HexHidden"))
-            {
-                item.GetComponent<Renderer>().material = hiddenMaterial;   
-                MeshCollider coll = item.GetComponent<MeshCollider>();
-                coll.convex = true;
-                coll.isTrigger = true;
-            }
-            PlayerController player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-            player.Reset();
-        }
-    }
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (!target)
+      {
+         GameObject[] targets = GameObject.FindGameObjectsWithTag("HexTarget");
+
+         Debug.Log(targets.Length);
+         target = targets[Random.Range(0, targets.Length)];
+
+         //target = GameObject.FindGameObjectWithTag("HexTarget");
+         //Debug.Log(target.name);
+
+         target.GetComponent<Renderer>().material = targetMaterial;
+         MeshCollider collider = target.GetComponent<MeshCollider>();
+         collider.convex = true;
+         collider.isTrigger = true;
+
+         foreach (var item in GameObject.FindGameObjectsWithTag("HexHidden"))
+         {
+            item.GetComponent<Renderer>().material = hiddenMaterial;
+            MeshCollider coll = item.GetComponent<MeshCollider>();
+            coll.convex = true;
+            coll.isTrigger = true;
+         }
+         PlayerController player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+         player.Reset();
+      }
+   }
 }
