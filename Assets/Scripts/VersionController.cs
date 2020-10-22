c8f56115bb3c47a88010d1932dedf8fc8f8ec55b
Erno Viitanen <erno.viitanen@student.lut.fi>
2020-10-22 11:25:59 +0300

    Level 7

---
+++ Assets/Scripts/VersionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionController : MonoBehaviour
+{
+
+    string version = "0.0.2";
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+       text.GetComponent<UnityEngine.UI.Text>().text = "v. " + version;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
