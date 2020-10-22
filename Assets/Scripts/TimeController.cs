ca144f9ca182836aecb957e8f9400ca598fd3d22
Erno Viitanen <erno.viitanen@student.lut.fi>
2020-10-22 12:21:58 +0300

    timer

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public GameObject text;
+    UnityEngine.UI.Text time;
+
+    public float timer;
+    public bool timeStarted = true;
+ 
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = text.GetComponent<UnityEngine.UI.Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (timeStarted == true) 
+        {
+            timer += Time.deltaTime;
+            ShowOnGui();
+            
+        }       
+    }
+    void ShowOnGui()
+    {
+        
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+    
+        //GUI.Label(new Rect(10,10,250,100), minutes + ":" + Mathf.RoundToInt(seconds));
+        time.text = FloatToTime(timer,"#0:00.00");
+    }
+    public string FloatToTime (float toConvert, string format){
+         switch (format){
+             case "00.0":
+                 return string.Format("{0:00}:{1:0}", 
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*10) % 10));//miliseconds
+             case "#0.0":
+                 return string.Format("{0:#0}:{1:0}", 
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*10) % 10));//miliseconds
+             
+             case "00.00":
+                 return string.Format("{0:00}:{1:00}", 
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*100) % 100));//miliseconds
+             case "00.000":
+                 return string.Format("{0:00}:{1:000}", 
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*1000) % 1000));//miliseconds
+             case "#00.000":
+                 return string.Format("{0:#00}:{1:000}", 
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*1000) % 1000));//miliseconds
+             case "#0:00":
+                 return string.Format("{0:#0}:{1:00}",
+                     Mathf.Floor(toConvert / 60),//minutes
+                     Mathf.Floor(toConvert) % 60);//seconds
+             case "#00:00":
+                 return string.Format("{0:#00}:{1:00}", 
+                     Mathf.Floor(toConvert / 60),//minutes
+                     Mathf.Floor(toConvert) % 60);//seconds
+             case "0:00.0":
+                 return string.Format("{0:0}:{1:00}.{2:0}",
+                     Mathf.Floor(toConvert / 60),//minutes
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*10) % 10));//miliseconds
+             case "#0:00.0":
+                 return string.Format("{0:#0}:{1:00}.{2:0}",
+                     Mathf.Floor(toConvert / 60),//minutes
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*10) % 10));//miliseconds
+             case "0:00.00":
+                 return string.Format("{0:0}:{1:00}.{2:00}",
+                     Mathf.Floor(toConvert / 60),//minutes
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*100) % 100));//miliseconds
+             case "#0:00.00":
+                 return string.Format("{0:#0}:{1:00}.{2:00}",
+                     Mathf.Floor(toConvert / 60),//minutes
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*100) % 100));//miliseconds
+             case "0:00.000":
+                 return string.Format("{0:0}:{1:00}.{2:000}",
+                     Mathf.Floor(toConvert / 60),//minutes
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*1000) % 1000));//miliseconds
+             case "#0:00.000":
+                 return string.Format("{0:#0}:{1:00}.{2:000}",
+                     Mathf.Floor(toConvert / 60),//minutes
+                     Mathf.Floor(toConvert) % 60,//seconds
+                     Mathf.Floor((toConvert*1000) % 1000));//miliseconds
+             
+         }
+         return "error";
+     }
+
+}
